88e2e33ace1871eeab5e24f3556c2f0fe0f73261
lauren-jones <105777927+lauren-jones@users.noreply.github.com>
2022-10-29 19:51:24 +0100

    Added home categories component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { HomeCategories } from "react-ecommerce-ui-kit";
+
 
 export const Home = () => {
- return (
-    <div>Hello from home</div>
- )
+   return (
+      <div>
+         <HomeCategories HomeCategories={[
+            { category: "Salts", image: "images/salts.webp", url: "/salts",},
+            { category: "Soaks", image: "images/soaks.webp", url: "/soaks",},
+            { category: "Masks", image: "images/masks.webp", url: "/masks",},
+            { category: "Gifts", image: "images/gifts.webp", url: "/gifts",}
+         ]}/>
+      </div>
+   )
 };
