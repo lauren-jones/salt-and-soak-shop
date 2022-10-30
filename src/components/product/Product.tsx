80dff71feb446e3ae5e03a01818afb3fb5f920ae
lauren-jones <105777927+lauren-jones@users.noreply.github.com>
2022-10-30 20:45:15 +0000

    Added new images

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom"
 import './product.css';
 import { saltsCategory } from "../../data/salts";
-import { ProductPage } from "react-ecommerce-ui-kit";
+import { ProductPage, RecommendedProducts } from "react-ecommerce-ui-kit";
 
 export default function Product() {
 
@@ -13,6 +13,9 @@ export default function Product() {
     return (
         <div>
             <ProductPage product={chosenProduct[0]}/>
+            <div className="recommended-products-section">
+                <RecommendedProducts title="You may also like" products={[saltsCategory[0], saltsCategory[1], saltsCategory[2]]} />
+            </div>
         </div>
     )
 }
